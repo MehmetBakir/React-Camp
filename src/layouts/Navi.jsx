bca0855ec31a59eb03fe6d768499b1185dc74266
Mehmet BAKIR <mehmet.b4kir@hotmail.com>
2022-10-20 00:28:20 +0300

    react-camp ilk commit

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,36 +1,28 @@
 import React from 'react'
 import CartSummary from './CartSummary'
-import { Button, Container, Dropdown, Menu } from 'semantic-ui-react'
+import { Button, Container, Menu } from 'semantic-ui-react'
 
 export default function Navi() {
-  return (
-    <div>
- <Menu inverted fixed>
-        <Container>
-        <Menu.Item
-          name='home'         
-        />
-        <Menu.Item
-          name='messages'        
-        />
+    return (
+        <div>
+            <Menu inverted fixed>
+                <Container>
+                    <Menu.Item
+                        name='home'
+                    />
+                    <Menu.Item
+                        name='messages'
+                    />
 
-        <Menu.Menu position='right'>
-          <Dropdown item text='Language'>
-            <Dropdown.Menu>
-              <Dropdown.Item>English</Dropdown.Item>
-              <Dropdown.Item>Russian</Dropdown.Item>
-              <Dropdown.Item>Spanish</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-
-          <Menu.Item>
-            <Button primary>Sign Up</Button>
-          </Menu.Item>
-        </Menu.Menu>
-        </Container>
-      </Menu>
+                    <Menu.Menu position='right'>
+                        <Menu.Item>
+                            <Button primary>Sign Up</Button>
+                        </Menu.Item>
+                    </Menu.Menu>
+                </Container>
+            </Menu>
 
 
-    </div>
-  )
+        </div>
+    )
 }
